b51b889180c950820a8d3cf0d3616175ea13c16d
Julian Schäfer <julian.david.schaefer@gmail.com>
2022-06-05 12:55:32 +0200

    Removed unneeded Endpoints

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,13 @@
 from flask import Flask, Response, request, json, jsonify
 from app.db import (
     GraphDatabaseConnection,
-    CompetencyInsertionFailed,
     CourseInsertionFailed,
     RetrievingCourseFailed,
     RetrievingCompetencyFailed,
-    CompetencyAndCourseInsertionFailed,
 )
 from app.store import Store
 from app.competency_extractors.competency_extractor import (
     PaperCompetencyExtractor,
-    DummyExtractor,
 )
 
 
@@ -115,116 +112,3 @@ def retrieve_competency():
     db.close()
 
     return jsonify(competencies)
-
-
-@app.route("/competency/<string:competency_name>", methods=["POST"])
-def create_competency(competency_name):
-    if request.headers.get("Content-Type") != "application/json":
-        return Response(
-            "Content-Type not supported! Expected type application/json",
-            status=400,
-            mimetype="application/json",
-        )
-
-    competency_body = json.loads(request.data).get("competency_body")
-    if not competency_body:
-        return Response(
-            "Body 'competency_body' is missing",
-            status=400,
-            mimetype="application/json",
-        )
-
-    db = GraphDatabaseConnection()
-    try:
-        competency = db.create_competency(
-            competency_name,
-            competency_body,
-        )
-    except CompetencyInsertionFailed as e:
-        return Response(f"error: {e}", status=400, mimetype="application/json")
-    db.close()
-
-    return jsonify(competency)
-
-
-@app.route("/courseCompetency", methods=["POST"])
-def create_competency_course_link():
-    if request.headers.get("Content-Type") != "application/json":
-        return Response(
-            "Content-Type not supported! Expected type application/json",
-            status=400,
-            mimetype="application/json",
-        )
-    competency_name = json.loads(request.data).get("competency_name")
-    competency_body = json.loads(request.data).get("competency_body")
-    course_name = json.loads(request.data).get("course_name")
-    course_body = json.loads(request.data).get("course_body")
-
-    if (
-        not competency_name
-        or not competency_body
-        or not course_name
-        or not course_body
-    ):
-        return Response(
-            "Body is incomplete, make sure to include 'competency_name', 'competency_body', 'course_name' and 'course_body'",
-            status=400,
-            mimetype="application/json",
-        )
-
-    db = GraphDatabaseConnection()
-    try:
-        competency_and_course = db.create_competecy_course_connection(
-            course_name,
-            course_body,
-            competency_name,
-            competency_body,
-        )
-    except CompetencyAndCourseInsertionFailed as e:
-        return Response(f"error: {e}", status=400, mimetype="application/json")
-    db.close()
-
-    return jsonify(competency_and_course)
-
-
-@app.route("/course/<string:course_name>/extract", methods=["POST"])
-def create_course_with_extraction(course_name: str):
-    if request.headers.get("Content-Type") != "application/json":
-        return Response(
-            "Content-Type not supported! Expected type application/json",
-            status=400,
-            mimetype="application/json",
-        )
-    course_body = json.loads(request.data).get("course_body")
-
-    if not course_body:
-        return Response(
-            "Body 'course_body' is missing",
-            status=400,
-            mimetype="application/json",
-        )
-
-    db = GraphDatabaseConnection()
-    extractor = DummyExtractor()
-    competencies = extractor.extract_competencies(course_body)
-
-    created_links = []
-
-    for competency in competencies:
-        competency_name = competency.get("name")
-        competency_body = competency.get("body")
-
-        try:
-            competency_and_course = db.create_competecy_course_connection(
-                course_name,
-                course_body,
-                competency_name,
-                competency_body,
-            )
-        except CompetencyAndCourseInsertionFailed:
-            continue
-        created_links.append(competency_and_course)
-
-    db.close()
-
-    return jsonify(created_links)
