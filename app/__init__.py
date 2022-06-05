c409a8902021c86ea382b1e4f7db3415028f04d0
Julian Schäfer <julian.david.schaefer@gmail.com>
2022-06-05 19:02:40 +0200

    Added flask-swagger-ui

--- app/__init__.py
+++ app/__init__.py
@@ -10,8 +10,21 @@ from app.competency_extractors.competency_extractor import (
     PaperCompetencyExtractor,
 )
 import xml.etree.ElementTree as ET
+from flask_swagger_ui import get_swaggerui_blueprint
 
-app = Flask(__name__)
+
+app = Flask(__name__, static_folder="../docs", static_url_path="/docs")
+
+
+SWAGGER_URL = "/api/docs"  # URL for exposing Swagger UI (without trailing '/')
+API_DEFINITION_FILE = "../../docs/api_spec_swagger.yml"  # Our API Definition
+
+swaggerui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL,  # Swagger UI static files will be mapped to '{SWAGGER_URL}/dist/'
+    API_DEFINITION_FILE,
+)
+
+app.register_blueprint(swaggerui_blueprint)
 
 
 @app.route("/")
